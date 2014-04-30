360775e751226da05c5c23aaacd49096c8746dbb
paulortm <paulortmarques@gmail.com>
2014-04-30 18:38:23 +0100

    Edit constructor call

--- src/ist/meic/pa/TracingEditor.java
+++ src/ist/meic/pa/TracingEditor.java
@@ -1,7 +1,17 @@
 package ist.meic.pa;
 
+import javassist.CannotCompileException;
+import javassist.expr.ConstructorCall;
 import javassist.expr.ExprEditor;
 
 public class TracingEditor extends ExprEditor {
 
+	public void edit(ConstructorCall c) {
+		try {
+			c.replace("{" + "System.out.println($0 + \"." + c.getMethodName()
+					+ "\");" + "$_ = $proceed($$);" + "}");
+		} catch (CannotCompileException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
