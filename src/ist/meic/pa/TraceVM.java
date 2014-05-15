300c50e00a43a07b2466ce83163d7e86b6ee195f
João Rodrigues <jrodriguesnum1@hotmail.com>
2014-05-15 12:10:48 +0100

    refactor TraceVM

--- src/ist/meic/pa/TraceVM.java
+++ src/ist/meic/pa/TraceVM.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.NotFoundException;
+import javassist.expr.ExprEditor;
 import javassist.tools.reflect.Loader;
 
 public class TraceVM {
@@ -14,28 +15,43 @@ public class TraceVM {
 	public static PrintStream out = System.err;
 
 	public static void main(String args[]) throws Throwable {
-		String mainClass = args[0];
-		List<String> mainArgs = new LinkedList<String>();
-		for (int i = 1; i < args.length; i++) {
-			mainArgs.add(args[i]);
-		}
-
-		// add translators
-		ClassPool cp = ClassPool.getDefault();
+		Loader loader = createLoader();
+		loader.delegateLoadingOf(Trace.class.getName());
+		loader.delegateLoadingOf(History.class.getName());
+		loader.delegateLoadingOf(TracingEditor.class.getName());
+		runWith(new TracingEditor(), loader, args);
+	}
+	
+	public static Loader createLoader() {
 		Loader loader = null;
 		try {
 			loader = new Loader();
-			loader.delegateLoadingOf(Trace.class.getName());
-			loader.delegateLoadingOf(History.class.getName());
-			loader.delegateLoadingOf(TracingEditor.class.getName());
 		} catch (CannotCompileException e) {
 			new RuntimeException(e);
 		} catch (NotFoundException e1) {
 			new RuntimeException(e1);
 		}
-		loader.addTranslator(cp, new TraceTranslator(new TracingEditor()));
-
-		loader.run(mainClass, removeFirstElm(args));
+		return loader;
+	}
+	
+	public static void runWith(ExprEditor editor, Loader loader, String args[]) {
+		String mainClass = args[0];
+		List<String> mainArgs = new LinkedList<String>();
+		for (int i = 1; i < args.length; i++) {
+			mainArgs.add(args[i]);
+		}
+		ClassPool cp = ClassPool.getDefault();
+		
+		try {
+			loader.addTranslator(cp, new TraceTranslator(new TracingEditor()));
+			loader.run(mainClass, removeFirstElm(args));
+		} catch (NotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (CannotCompileException e) {
+			throw new RuntimeException(e);
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static String[] removeFirstElm(String[] array) {
